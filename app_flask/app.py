4c2e9e9e43caa49fd9182d9dee13a829f3fa27de
asidorenko@mobiledep.ru <asidorenko@mobiledep.ru>
2024-10-03 12:28:23 +0300

    add app_py

---
+++ app_flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+  return f'User {username}'
+
+@app.route('/health')
+def health():
+    return '{"status": "OK"}'
+
+app.run(debug=True, host='0.0.0.0')
