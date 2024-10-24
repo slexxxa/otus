1e13c46d4455dc465b553807d223c0776064b92b
root <root@node3.otus.loc>
2024-10-25 00:03:26 +0300

    add GET to app_flask

--- app_flask/app.py
+++ app_flask/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, json
 import psycopg2
 
 
-def psql(method, username, firstname, lastname, email, phone):
+def psql(method, uid=0,  username=0, firstname=0, lastname=0, email=0, phone=0):
+    global output
     conn = psycopg2.connect(
         dbname="otus",
         user="postgres",
@@ -14,16 +15,32 @@ def psql(method, username, firstname, lastname, email, phone):
     cur = conn.cursor()
 
     # conn.autocommit = True
+    if method == "GET":
+        cur.execute("""
+                SELECT username, firstname, lastname, email, phone FROM users WHERE id = %s;""", (uid, )
+                    )
+        row = cur.fetchone()
+        output = json.jsonify(
+                      id=uid,
+                      username=row[0],
+                      firstName=row[1],
+                      lastName=row[2],
+                      email=row[3],
+                      phone=row[4]
+                                )
 
-    cur.execute("""
-        INSERT INTO users (username, firstname, lastname, email, phone) 
-        VALUES (%s, %s, %s, %s, %s);""", (username, firstname, lastname, email, phone)
-                )
+    if method == "POST":
+        cur.execute("""
+            INSERT INTO users (username, firstname, lastname, email, phone) 
+            VALUES (%s, %s, %s, %s, %s);""", (username, firstname, lastname, email, phone)
+                    )
+        output = ''
     conn.commit()
 
     cur.close()
     conn.close()
 
+    return output
 
 app = Flask(__name__)
 
@@ -33,9 +50,10 @@ def hello_world():
     return 'Hello, World!'
 
 
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-#     return f'User {username}'
+@app.route('/api/v1/user/<uid>', methods=['GET'])
+def show_user_profile(uid):
+    # uid = int(uid)
+    return psql("GET", uid)
 
 
 @app.route('/health')
@@ -43,17 +61,17 @@ def health():
     return '{"status": "OK"}'
 
 
-@app.route('/user', methods=['POST'])
+@app.route('/api/v1/user', methods=['POST'])
 def user():
     request_data = request.get_json()
 
     username = request_data['username']
-    firstname = request_data['firstname']
-    lastname = request_data['lastname']
+    firstname = request_data['firstName']
+    lastname = request_data['lastName']
     email = request_data['email']
     phone = request_data['phone']
-    psql("insert", username, firstname, lastname, email, phone)
-    return f'Username {username}, firstname {firstname}'
+    return psql("POST", 0, username, firstname, lastname, email, phone)
 
 
 app.run(debug=True, host='0.0.0.0', port=8000)
+
