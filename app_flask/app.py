19c5401dd11eb924759623e9686f0e4b44af7ce0
root <root@node3.otus.loc>
2024-10-28 21:57:01 +0300

    add PUT and some

--- app_flask/app.py
+++ app_flask/app.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, json
-import psycopg2
-
+import psycopg2, os
 
 def psql(method, uid=0, username=0, firstname=0, lastname=0, email=0, phone=0):
     global output
     conn = psycopg2.connect(
-        dbname="otus",
-        user="postgres",
-        password="12345678",
-        host="10.169.44.141",
-        port="30490"
+        dbname=os.environ["PGDBNAME"],
+        user=os.environ["PGUSER"],
+        password=os.environ["PGPASSWORD"],
+        host=os.environ["PGHOST"],
+        port=os.environ.get("PGPORT", "5432")
     )
 
     cur = conn.cursor()
@@ -32,6 +31,17 @@ def psql(method, uid=0, username=0, firstname=0, lastname=0, email=0, phone=0):
     if method == "DELETE":
         cur.execute("""
                 DELETE FROM users WHERE id = %s;""", (uid,)
+                    )
+        output = ''
+
+    if method == "PUT":
+        cur.execute("""
+                UPDATE users SET 
+                firstname = %s, 
+                lastname = %s,
+                email = %s,
+                phone = %s
+                WHERE id = %s;""", (firstname, lastname, email, phone, uid)
                     )
         output = ''
 
@@ -66,6 +76,17 @@ def get_user(uid):
 @app.route('/api/v1/user/<uid>', methods=['DELETE'])
 def delete_user(uid):
     return psql("DELETE", uid), 204
+
+
+@app.route('/api/v1/user/<uid>', methods=['PUT'])
+def update_user(uid):
+    request_data = request.get_json()
+
+    firstname = request_data['firstName']
+    lastname = request_data['lastName']
+    email = request_data['email']
+    phone = request_data['phone']
+    return psql("PUT", uid, 0, firstname, lastname, email, phone)
 
 
 @app.route('/health')
